a97457d65fae3802e261e7cbcbaffbe40a77ea18
Virginia Márquez <109605744+Virgi2112@users.noreply.github.com>
2023-05-21 21:01:52 -0400

    Clase #2 Bucles en JS

---
+++ Clase #2 Bucles en JS.js
@@ -0,0 +1,27 @@
+// Bucles en JavaScript
+//3 elementos: 1. Inicialización 2.Condición de permanencia 3. Actualización de la Variable que controla esa condición.
+
+// bucle While (mientras)
+let condicion 
+
+while (condicion){
+    codigo
+}
+
+let i =1;
+
+while (i < 11) {
+    console.log(i);
+    i++
+}
+
+// Con funciones
+function buclewhile(num){
+    let i=0;
+    while (i < num ){
+        console.log(i);
+        i++;
+    }
+}
+
+buclewhile(11);
